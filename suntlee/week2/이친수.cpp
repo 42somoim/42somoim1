8e9bfe5368148914226c683401079751a34a78db
Suntae Lee <suntlee@c5r5s5.42seoul.kr>
2020-07-28 17:39:24 +0900

    Change result type int to long

--- suntlee/week2/이친수.cpp
+++ suntlee/week2/이친수.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 
+int main()
+{
+	int n;
+	std::cin >> n;
+
+	long dp[n+1][2];
+
+	dp[1][0] = 0;
+	dp[1][1] = 1;
+	
+	for (int i = 2; i < n + 1; i++){
+		dp[i][0] = dp[i-1][0] + dp[i-1][1];
+		dp[i][1] = dp[i-1][0];
+	}
+	std::cout << dp[n][0] + dp[n][1];
+	return 0;
+}
